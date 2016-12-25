2f68b2b2da9efe30994e9cf53eff36dd74dec895
TheMolkaPL <themolkapl@gmail.com>
2016-12-25 20:15:31 +0100

    Intial commit

---
+++ src/main/java/pl/themolka/arcade/ArcadePlugin.java
@@ -0,0 +1,22 @@
+package pl.themolka.arcade;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * The Arcade main class
+ */
+public final class ArcadePlugin extends JavaPlugin {
+    @Override
+    public void onLoad() {
+        // TODO body
+    }
+
+    @Override
+    public void onEnable() {
+        // TODO body
+    }
+
+    public void onDisable() {
+        // TODO body
+    }
+}
