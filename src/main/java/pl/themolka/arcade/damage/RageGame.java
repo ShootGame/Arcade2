b516c09a4d58b6d987bc8644c3967c2475cd90e6
TheMolkaPL <themolkapl@gmail.com>
2018-04-29 22:21:27 +0200

    Filter rage module

--- src/main/java/pl/themolka/arcade/damage/RageGame.java
+++ src/main/java/pl/themolka/arcade/damage/RageGame.java
@@ -1,50 +1,60 @@
 package pl.themolka.arcade.damage;
 
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import pl.themolka.arcade.attribute.BukkitAttributeKey;
 import pl.themolka.arcade.config.Ref;
+import pl.themolka.arcade.filter.Filter;
+import pl.themolka.arcade.filter.Filters;
 import pl.themolka.arcade.game.Game;
 import pl.themolka.arcade.game.GameModule;
+import pl.themolka.arcade.game.IGameConfig;
 import pl.themolka.arcade.game.IGameModuleConfig;
 
 public class RageGame extends GameModule {
-    private final double damage;
+    private static final BukkitAttributeKey MAX_HEALTH = new BukkitAttributeKey(Attribute.GENERIC_MAX_HEALTH);
+    private static final double DEFAULT_MAX_HEALTH = 1_000_000D;
 
-    protected RageGame(Config config) {
-        this.damage = config.damage().getOrDefault(Config.DEFAULT_DAMAGE);
-    }
+    private final Filter filter;
 
-    public double getDamage() {
-        return this.damage;
+    protected RageGame(Game game, IGameConfig.Library library, Config config) {
+        this.filter = Filters.secure(library.getOrDefine(game, config.filter().getIfPresent()));
     }
 
     @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
     public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        Entity victim = event.getEntity();
         Entity damager = event.getDamager();
-        if (damager instanceof Player) {
-            this.damage(event);
-        } else if (damager instanceof Projectile && ((Projectile) damager).getShooter() instanceof Player) {
-            this.damage(event);
+
+        if (victim instanceof LivingEntity && this.filter.filter(victim, damager).isNotFalse()) {
+            if (damager instanceof Player) {
+                this.damage(event, (LivingEntity) victim);
+            } else if (damager instanceof Projectile && ((Projectile) damager).getShooter() instanceof Player) {
+                this.damage(event, (LivingEntity) victim);
+            }
         }
     }
 
-    private void damage(EntityDamageByEntityEvent event) {
-        event.setDamage(EntityDamageEvent.DamageModifier.BASE, this.getDamage());
+    private void damage(EntityDamageByEntityEvent event, LivingEntity victim) {
+        AttributeInstance maxHealth = victim.getAttribute(MAX_HEALTH.getBukkit());
+        event.setDamage(EntityDamageEvent.DamageModifier.BASE, maxHealth != null ? maxHealth.getValue()
+                                                                                 : DEFAULT_MAX_HEALTH);
     }
 
     public interface Config extends IGameModuleConfig<RageGame> {
-        double DEFAULT_DAMAGE = 10_000D;
-
-        default Ref<Double> damage() { return Ref.empty(); }
+        default Ref<Filter.Config<?>> filter() { return Ref.empty(); }
 
         @Override
         default RageGame create(Game game, Library library) {
-            return new RageGame(this);
+            return new RageGame(game, library, this);
         }
     }
 }
--- src/main/java/pl/themolka/arcade/damage/RageGameParser.java
+++ src/main/java/pl/themolka/arcade/damage/RageGameParser.java
@@ -2,6 +2,7 @@ package pl.themolka.arcade.damage;
 
 import pl.themolka.arcade.config.Ref;
 import pl.themolka.arcade.dom.Node;
+import pl.themolka.arcade.filter.Filter;
 import pl.themolka.arcade.game.GameModuleParser;
 import pl.themolka.arcade.parser.InstallableParser;
 import pl.themolka.arcade.parser.Parser;
@@ -14,7 +15,7 @@ import pl.themolka.arcade.parser.Produces;
 @Produces(RageGame.Config.class)
 public class RageGameParser extends GameModuleParser<RageGame, RageGame.Config>
                             implements InstallableParser {
-    private Parser<Double> damageParser;
+    private Parser<Ref> filterParser;
 
     public RageGameParser() {
         super(RageGame.class);
@@ -28,18 +29,15 @@ public class RageGameParser extends GameModuleParser<RageGame, RageGame.Config>
     @Override
     public void install(ParserContext context) throws ParserNotSupportedException {
         super.install(context);
-        this.damageParser = context.type(Double.class);
+        this.filterParser = context.type(Ref.class);
     }
 
     @Override
     protected ParserResult<RageGame.Config> parseNode(Node node, String name, String value) throws ParserException {
-        double damage = this.damageParser.parseWithDefinition(node, name, value).orDefault(RageGame.Config.DEFAULT_DAMAGE);
-        if (damage <= 0) {
-            throw this.fail(node, name, value, "Damage must be positive (greater than 0)");
-        }
+        Ref<Filter.Config<?>> filter = this.filterParser.parse(node.property("filter")).orDefault(Ref.empty());
 
         return ParserResult.fine(node, name, value, new RageGame.Config() {
-            public Ref<Double> damage() { return Ref.ofProvided(damage); }
+            public Ref<Filter.Config<?>> filter() { return filter; }
         });
     }
 }
